5ccd0d174e17997bc54d5fc6fcd80a08c4a91aae
Yuki <lalala01192000@gmail.com>
2012-09-18 21:37:15 +0900

    first commit

---
+++ test/content/browserOverlay.js
@@ -0,0 +1,18 @@
+/**
+ * XULSchoolChrome namespace.
+ */
+if ("undefined" == typeof(XULSchoolChrome)) {
+  var XULSchoolChrome = {};
+};
+
+/**
+ * Controls the browser overlay for the Hello World extension.
+ */
+XULSchoolChrome.BrowserOverlay = {
+  
+};
+
+function invokeAlert() {
+    window.alert("Hello World!");
+};
+
